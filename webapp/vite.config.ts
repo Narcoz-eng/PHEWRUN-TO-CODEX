ded70d3b3b36dc0a951469ba2c6322011ab22b31
Narcoz-eng <rengarro@gmail.com>
2026-02-21 17:30:58 +0200

    FIX V0.11

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { vibecodePlugin } from "@vibecodeapp/webapp/plugin";
 import path from "path";
 
